3df4eb6fe16b727b843c07dcaca791133340f67b
MinGRn <MinGRn97@gmail.com>
2018-06-20 09:07:47 +0800

    未做功能操作;

--- springcloud-ribbon-consumer/src/main/java/com/mingrn/consumer/web/ConsumerController.java
+++ springcloud-ribbon-consumer/src/main/java/com/mingrn/consumer/web/ConsumerController.java
@@ -1,17 +1,18 @@
 package com.mingrn.consumer.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping
 public class ConsumerController {
 
-	@Autowired
-	RestTemplate restTemplate;
+	@Resource
+	private RestTemplate restTemplate;
 
 	/**
 	 * 注意: 这里的访问地址名不是ip,而是服务应用名称,在服务治理框架中,这是一个非常主要的特性
