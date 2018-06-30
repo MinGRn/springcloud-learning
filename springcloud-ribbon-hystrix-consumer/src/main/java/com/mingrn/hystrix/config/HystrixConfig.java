f997175dcd2adc75b81f4ca0e1940d1c607cb66a
MinGRn <MinGRn97@gmail.com>
2018-06-30 17:04:30 +0800

    - 新增依赖:spring-boot-starter-actuator; - 新增Hystrix配置类; 增加如上两项为 Hystrix仪表盘监控 做准备

---
+++ springcloud-ribbon-hystrix-consumer/src/main/java/com/mingrn/hystrix/config/HystrixConfig.java
@@ -0,0 +1,36 @@
+package com.mingrn.hystrix.config;
+
+import com.netflix.hystrix.contrib.javanica.aop.aspectj.HystrixCommandAspect;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Hystrix 配置
+ *
+ * @author MinGRn <br > 2018/6/30 17:02
+ */
+@Configuration
+public class HystrixConfig {
+
+	/**
+	 * 用来拦截处理HystrixCommand注解
+	 */
+	@Bean
+	public HystrixCommandAspect hystrixCommandAspect() {
+		return new HystrixCommandAspect();
+	}
+
+
+	/**
+	 * 用来像监控中心Dashboard发送stream信息
+	 */
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+		registration.addUrlMappings("/hystrix.stream");
+		return registration;
+	}
+
+}
