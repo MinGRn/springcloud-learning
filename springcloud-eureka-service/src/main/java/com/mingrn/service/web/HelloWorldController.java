98ba0de7f1dd76e542f1168197317fb353a72db7
MinGRn <MinGRn97@gmail.com>
2018-07-05 19:06:18 +0800

    修改Controller名称;

--- springcloud-eureka-service/src/main/java/com/mingrn/service/web/HelloWorldController.java
+++ springcloud-eureka-service/src/main/java/com/mingrn/service/web/HelloWorldController.java
@@ -1,15 +1,12 @@
 package com.mingrn.service.web;
 
+import com.mingrn.service.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.serviceregistry.Registration;
-import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.util.List;
@@ -21,43 +18,22 @@ public class HelloWorldController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(HandlerExceptionResolver.class);
 
-	/**
-	 * 服务发现客户端
-	 */
-	@Autowired
-	private DiscoveryClient discoveryClient;
-
-	@GetMapping("/index")
-	public String index() {
-		ServiceInstance serviceInstance = serviceInstance();
-		LOGGER.info("provider service, host：{}，service_id：{}", serviceInstance.getHost(), serviceInstance.getServiceId());
-		return "provider service, host：" + serviceInstance.getHost() + "，service_id：" + serviceInstance.getServiceId();
+	@GetMapping("/hello1")
+	public String hello1(@RequestParam String name) {
+		return "Hello" + name;
 	}
 
-	/**
-	 * 模拟服务阻塞
-	 */
-	@GetMapping("/serviceBlock")
-	public String serviceBlock() throws InterruptedException {
-		ServiceInstance serviceInstance = serviceInstance();
-
-		//让线程等待几秒
-		int sleepTime = new Random().nextInt(3000);
-		LOGGER.info(">>> sleepTime: " + sleepTime);
-		Thread.sleep(sleepTime);
-
-		LOGGER.info("provider service, host：{}，service_id：{}", serviceInstance.getHost(), serviceInstance.getServiceId());
-		return "provider service, host：" + serviceInstance.getHost() + "，service_id：" + serviceInstance.getServiceId();
+	@GetMapping("/hello2")
+	public User hello2(@RequestHeader String name, @RequestHeader Integer age) {
+		return new User(name, age);
 	}
 
-
-	/**
-	 * 获取当前服务的服务实例
-	 *
-	 * @author MinGRn <br > 2018/6/16 15:43
-	 */
-	private ServiceInstance serviceInstance() {
-		List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("hello-service");
-		return (serviceInstanceList != null && serviceInstanceList.size() > 0) ? serviceInstanceList.get(0) : null;
+	@PostMapping("/hello3")
+	public String hello3(@RequestBody User user) {
+		StringBuffer sb = new StringBuffer("Hello")
+				.append(user.getName())
+				.append(",")
+				.append(user.getAge());
+		return sb.toString();
 	}
 }
---
+++ springcloud-eureka-service/src/main/java/com/mingrn/service/web/IndexController.java
@@ -0,0 +1,63 @@
+package com.mingrn.service.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.util.List;
+import java.util.Random;
+
+@RestController
+@RequestMapping
+public class IndexController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(HandlerExceptionResolver.class);
+
+	/**
+	 * 服务发现客户端
+	 */
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@GetMapping("/index")
+	public String index() {
+		ServiceInstance serviceInstance = serviceInstance();
+		LOGGER.info("provider service, host：{}，service_id：{}", serviceInstance.getHost(), serviceInstance.getServiceId());
+		return "provider service, host：" + serviceInstance.getHost() + "，service_id：" + serviceInstance.getServiceId();
+	}
+
+	/**
+	 * 模拟服务阻塞
+	 */
+	@GetMapping("/serviceBlock")
+	public String serviceBlock() throws InterruptedException {
+		ServiceInstance serviceInstance = serviceInstance();
+
+		//让线程等待几秒
+		int sleepTime = new Random().nextInt(3000);
+		LOGGER.info(">>> sleepTime: " + sleepTime);
+		Thread.sleep(sleepTime);
+
+		LOGGER.info("provider service, host：{}，service_id：{}", serviceInstance.getHost(), serviceInstance.getServiceId());
+		return "provider service, host：" + serviceInstance.getHost() + "，service_id：" + serviceInstance.getServiceId();
+	}
+
+
+	/**
+	 * 获取当前服务的服务实例
+	 *
+	 * @author MinGRn <br > 2018/6/16 15:43
+	 */
+	private ServiceInstance serviceInstance() {
+		List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("hello-service");
+		return (serviceInstanceList != null && serviceInstanceList.size() > 0) ? serviceInstanceList.get(0) : null;
+	}
+}
